d33c379ef1a15239521a937d29efc88e59a8fd68
yyfbe <2861112448@qq.com>
2020-10-26 20:40:44 +0800

    [feature]生成player

--- src/framework/PlayerGenerator.java
+++ src/framework/PlayerGenerator.java
@@ -1,9 +1,25 @@
 package framework;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Objects;
 
 class PlayerGenerator {
-    public ArrayList<AbstractPlayer> generate(){
-
+    private static final String PLAYER_FILE_PATH = "src/players";
+    public ArrayList<AbstractPlayer> generate() {
+        ArrayList<AbstractPlayer> players = new ArrayList<>();
+        File playerFile = new File(PLAYER_FILE_PATH);
+        for (File file : Objects.requireNonNull(playerFile.listFiles())) {
+            if (file.getName().contains(".java")) {
+                String className = file.getName().replace(".java", "");
+                try {
+                    Class<? extends AbstractPlayer> clazz = (Class<? extends AbstractPlayer>) Class.forName(PLAYER_FILE_PATH + className);
+                    players.add(clazz.newInstance());
+                } catch (ClassNotFoundException | IllegalAccessException | InstantiationException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return players;
     }
 }
